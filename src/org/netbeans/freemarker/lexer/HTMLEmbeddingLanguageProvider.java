9be61f6bec230bece4023dd81705345d5cda5298
Pedro Hidalgo <pedroantoniohidalgo@gmail.com>
2016-10-31 17:53:19 -0400

    #resolve issue: #12 HTML code completion is broken

--- src/org/netbeans/freemarker/lexer/HTMLEmbeddingLanguageProvider.java
+++ src/org/netbeans/freemarker/lexer/HTMLEmbeddingLanguageProvider.java
@@ -1,21 +1,26 @@
 package org.netbeans.freemarker.lexer;
 
 import org.netbeans.api.editor.mimelookup.MimeLookup;
+import org.netbeans.api.html.lexer.HTMLTokenId;
 import org.netbeans.api.lexer.InputAttributes;
 import org.netbeans.api.lexer.Language;
 import org.netbeans.api.lexer.LanguagePath;
 import org.netbeans.api.lexer.Token;
-import org.netbeans.freemarker.panel.FTLPanel;
 import org.netbeans.spi.lexer.LanguageEmbedding;
 import org.netbeans.spi.lexer.LanguageProvider;
-import org.openide.util.NbPreferences;
 import org.openide.util.lookup.ServiceProvider;
 
 @ServiceProvider(service = LanguageProvider.class)
 public class HTMLEmbeddingLanguageProvider extends LanguageProvider {
+    
+    Language ftlLanguage;
 
     @Override
     public Language<?> findLanguage(String mimeType) {
+        if (mimeType.equals("text/x-ftl")) {
+            return HTMLTokenId.language();
+        }
+
         return null;
     }
 
@@ -23,16 +28,24 @@ public class HTMLEmbeddingLanguageProvider extends LanguageProvider {
     public LanguageEmbedding<?> findLanguageEmbedding(Token<?> token,
             LanguagePath languagePath, InputAttributes inputAttributes) {
 
-        String embeddedMime = NbPreferences.forModule(FTLPanel.class).get("embeddedMime", "text/html");
+        initilizeLanguage();
 
-        Language<?> embeddedLanguage = MimeLookup.getLookup(embeddedMime).lookup(Language.class);
-
-        if (embeddedLanguage != null && languagePath.mimePath().equals("text/x-ftl")) {
-            if (token.id().name().startsWith("STATIC_TEXT")) {
-                return LanguageEmbedding.create(embeddedLanguage, 0, 0, true);
+        if (languagePath.mimePath().equals("text/html")) {
+            if (token.id().name().equals("TEXT")) {
+                LanguageEmbedding languageEmbedding = LanguageEmbedding.create(ftlLanguage, 0, 0, false);
+                return languageEmbedding;
             }
         }
+
         return null;
+    }
+
+    private void initilizeLanguage() {
+        if (ftlLanguage == null) {
+
+            ftlLanguage = MimeLookup.getLookup(
+                    "text/x-ftl").lookup(Language.class);
+        }
     }
 
 }
